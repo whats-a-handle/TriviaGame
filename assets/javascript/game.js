dcc3c4c34e82d0804f245db98d3ded5d77e6f4f1
whats-a-handle <bromanbroman54@yahoo.com>
2017-12-23 15:57:56 -0800

    Converted for loops to maps Added addQuestion function on Game to add questions to questionPool questionText is now updated on the page depending on the selected question nextQuestion function randomly selects a question and removes the selected question from the pool to prevent dupes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,58 +5,78 @@ function createGame(){
 	questionPool:[],
 	correctAnswers : 0,
 	incorrectAnswers: 0,
+	questionTextElement: $(".question-text"),
+	answerContainerElement: $(".answer-button-container"),
+	addQuestion : function(question){
+		this.questionPool.push(question);
+	},
 	nextQuestion:function(){
-		//do stuff after our timer is up
+		
+		const randomQuestionIndex = Math.floor(Math.random() * this.questionPool.length);
+		const question = this.questionPool[randomQuestionIndex];
+
+		this.questionPool.splice(randomQuestionIndex, 1);
+
+		return question;
 	},
 	checkSelectedAnswer:function(selection,question){
 		//check if the selected answer is the correct answer
 	},
 		//currently only updates container with buttons for answers
 		//next is updating the actual header text to be the question
+	emptyQuestionElement:function(){
+
+
+
+	},
+	//Using "this" in a map refers to the array i think and not the actual object
 	updateQuestionElement:function(question){
+		const game = this;
 
-		const questionElements = question.generateQuestionElements();
+		game.questionTextElement.append("<h1>" + question.questionText + "</h1>");
+		question.questionElements.map(function(questionElement){
 
-		for(let i = 0; i < questionElements.length; i++){
+			game.answerContainerElement.append(questionElement);
 
-			$(".answer-button-container").append(questionElements[i]);
-		}
-		
-
+		});
+				
 	}
 
 
 }
+
 
 	return Game;
 }
 
 
+//;
 
-
-function createQuestion(question,answer,answerPool){
+function createQuestion(questionText,answer,answerPool){
 
 	const Question = {
 
-		question:question,
+		questionText:questionText,
 		answer:answer,
 		answerPool: answerPool,
-		questionElements:this.generateQuestionElements,
-
+		questionElements:undefined,
 		generateQuestionElements: function(){
-			const question = this;
 			const questionElements = [];
-			for(let i = 0; i < this.answerPool.length; i++){
-				const questionElement = "<p><a class=\"btn btn-primary btn-lg\" role=\"button\">"+ question.answerPool[i] +"</a></p>";
 
-				questionElements.push(questionElement);
-			}
-			
+			answerPool.map(function(item){
+
+				questionElements.push("<p><a class=\"btn btn-primary btn-lg\" role=\"button\">"+ item + "</p>");
+
+			});
+				
 			return questionElements;
-		}
-		
-		
+		},
+
+				
 	}
+	//I really don't like this.
+	//I was unable to invoke the function on object creation, so i will have to do it outside
+	Question.questionElements = Question.generateQuestionElements();
 
 	return Question;
 }
@@ -65,14 +85,14 @@ function createQuestion(question,answer,answerPool){
 
 
 $( document ).ready(function(){
-	
-	let question_one = createQuestion("Who Killed Roger Rabbit?","Global Warming", ["global warming","babe ruth","charlie chaplin"]);
-	//testing the ambiguous object
-	console.log(question_one);
-	
+		
 	const game = createGame();
 	//create buttons based off of the answer list in our question object
-	game.updateQuestionElement(question_one);
+	game.addQuestion(createQuestion("Question 1?","No1", ["Yes1","No1"]));
+	game.addQuestion(createQuestion("Question 2?", "Yes2", ["Yes2","No2"]));
+	game.addQuestion(createQuestion("Question 3?", "Yes3", ["Yes3","No3"]));
+	game.updateQuestionElement(game.nextQuestion());
+
 
 
 });
