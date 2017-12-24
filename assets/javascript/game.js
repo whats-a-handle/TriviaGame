0a3ebe08da952f8636255855e9b38f69ef64f1d4
whats-a-handle <bromanbroman54@yahoo.com>
2017-12-23 23:31:11 -0800

    Countdown timer added and added to html alerts will no longer stack up and will be cleared properly Super rough and working Need more time to refactor everything

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ function createGame(){
 
 	questionPool:[],
 	timeoutPool:[],
+	intervalPool:[],
 	correctAnswers : 0,
 	incorrectAnswers: 0,
 	skippedAnswers: 0,
@@ -18,7 +19,9 @@ function createGame(){
 
 	gameOver: function(){
 		const game = this;
-		game.alertContainer.empty();
+		clearInterval(game.intervalPool[game.intervalPool.length-1]);
+		$(".countdown").empty();
+		//game.alertContainer.empty();
 		if(game.correctAnswers > (game.incorrectAnswers + game.skippedAnswers)){
 			console.log("You win!");
 			game.questionTextElement.html("<h1>You WIN!</div>");
@@ -33,18 +36,20 @@ function createGame(){
 		game.answerContainerElement.append("<h3>Correct: " + game.correctAnswers +"</h3>");
 		game.answerContainerElement.append("<h3>Incorrect: " + game.incorrectAnswers +"</h3>");
 		game.answerContainerElement.append("<h3>Skipped: " + game.skippedAnswers +"</h3>");
+		game.answerContainerElement.append("<p><a class=\"btn btn-primary btn-lg btn-restart\" role=\"button\">RESTART</p>");
 
 		console.log("Correct: " + game.correctAnswers + " Incorrect: " + game.incorrectAnswers + " Skipped: " + game.skippedAnswers);
 
 	},
 	nextQuestion:function(){
 		const game = this;
+		game.countdown();
 
 		if(game.previousSelection === undefined){
 				game.skippedAnswers++;
 				game.alertContainer.append("<div class=\"alert alert-danger\">"+
   			"<strong>SLOWPOKE! The correct answer was " + game.currentAnswer + "</strong></div>");
-				setTimeout(function(){game.alertContainer.empty()},3000);
+				setTimeout(function(){game.alertContainer.empty()},3500);
 			
 			}
 
@@ -73,6 +78,8 @@ function createGame(){
 	},
 	checkIsCorrect:function(selection,questionAnswer){
 		const game = this;
+
+		game.alertContainer.empty();
 		if(selection === questionAnswer){
 			game.correctAnswers++;
 			game.alertContainer.append("<div class=\"alert alert-success\">"+
@@ -106,11 +113,47 @@ function createGame(){
 
 		});
 				
-	}
+	},
+
+	restartGame: function(){
+		const game = this;
+		game.questionPool=[];
+		game.timeoutPool=[];
+		game.intervalPool = [];
+		game.correctAnswers = 0;
+		game.incorrectAnswers = 0;
+		game.skippedAnswers = 0;
+		game.previousSelection =  "default";
+		game.currentAnswer = undefined;
+
+		game.clearQuestionAnswerElements();
+		game.alertContainer.empty();
+		Game.addQuestion(createQuestion("Who Killed Roger Rabbit?","That creepy guy with the trenchcoat", 
+												["Bob Marley","Jessica Rabbit", "Talking car","That creepy guy with the trenchcoat"]));
+		Game.addQuestion(createQuestion("Your favorite colour is blue", "Yes", ["Yes","No"]));
+		Game.addQuestion(createQuestion("What happened on December 23rd, 1979?", "Soviet tanks went into Afghanistan", 
+												["Swedish troops occupy Norway","The Beatles released their first album",
+												"Soviet tanks went into Afghanistan","The first computer was created"]));
+	},
+	countdown:function(){
+		const game = this;
+		let i = 10;
+		clearInterval(game.intervalPool[game.intervalPool.length-1]);
+		game.intervalPool.push(setInterval(function(){
+
+			$(".countdown").html("<h1>" + --i + "</h1>");
+		},1000));
+	
+}
+
+
+
+
 
 
 }
-
+	
+	Game.restartGame();
 
 	return Game;
 }
@@ -152,10 +195,7 @@ function createQuestion(questionText,answer,answerPool){
 	$( document ).ready(function(){
 
 		const game = createGame();
-		game.addQuestion(createQuestion("Question 1?","No1", ["Yes1","No1"]));
-		game.addQuestion(createQuestion("Question 2?", "Yes2", ["Yes2","No2"]));
-		game.addQuestion(createQuestion("Question 3?", "Yes3", ["Yes3","No3"]));
-
+		
 		game.nextQuestion();
 		game.previousSelection = undefined;
 
@@ -163,8 +203,19 @@ function createQuestion(questionText,answer,answerPool){
 		 	clearTimeout(game.timeoutPool[game.timeoutPool.length-1]);
 		 	game.previousSelection = $(this).attr("value");
 		 	game.checkIsCorrect(game.previousSelection,game.currentAnswer);
-		 	setTimeout(function(){game.alertContainer.empty()},3000);
+		 	setTimeout(function(){game.alertContainer.empty()},3500);
 		 	game.nextQuestion();
+
+		 });
+
+		 $(document).on("click",".btn-restart",function(){
+
+		 	game.restartGame();
+		 	game.nextQuestion();
+		 	game.previousSelection = undefined;
+
+
+
 
 		 });
 
