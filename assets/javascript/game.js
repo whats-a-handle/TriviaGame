eabd44ccea9d44cdb3cb086c1152e5b437d98b30
whats-a-handle <bromanbroman54@yahoo.com>
2017-12-23 17:06:22 -0800

    game can be played and won or lost now gameOver function checks score and logs win or lost for now checkIsCorrect checks if the selected answer matches the games current answer and increments score nextQuestion now calls updateQuestionElement if there are more questions left in the pool nextQuestion will not select a new question if the pool is empty. It calls the gameOver function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,27 +5,63 @@ function createGame(){
 	questionPool:[],
 	correctAnswers : 0,
 	incorrectAnswers: 0,
+	skippedAnswers: 0,
+	currentAnswer: undefined,
 	questionTextElement: $(".question-text"),
 	answerContainerElement: $(".answer-button-container"),
 	addQuestion : function(question){
 		this.questionPool.push(question);
 	},
-	nextQuestion:function(){
-		
-		const randomQuestionIndex = Math.floor(Math.random() * this.questionPool.length);
-		const question = this.questionPool[randomQuestionIndex];
 
-		this.questionPool.splice(randomQuestionIndex, 1);
+	gameOver: function(){
+		const game = this;
 
-		return question;
+		if(game.correctAnswers > (game.incorrectAnswers + game.skippedAnswers)){
+			console.log("You win!");
+			//Need to update by creating a win screen
+		}
+		else{
+			console.log("You LOSE");
+			//Need to update by creating a win screen
+		}
+
 	},
-	checkSelectedAnswer:function(selection,question){
-		//check if the selected answer is the correct answer
+	nextQuestion:function(){
+		const game = this;
+
+		game.clearQuestionAnswerElements()
+		if(game.questionPool.length > 0){
+			const randomQuestionIndex = Math.floor(Math.random() * game.questionPool.length);
+			const question = game.questionPool[randomQuestionIndex];
+			game.currentAnswer = question.answer;
+			game.questionPool.splice(randomQuestionIndex, 1);
+
+			game.updateQuestionElement(question);
+
+		}
+		else{
+
+			game.gameOver();
+		}
+		
+	},
+	checkIsCorrect:function(selection,questionAnswer){
+		const game = this;
+		if(selection === questionAnswer){
+			game.correctAnswers++;
+		}
+		else{
+			game.incorrectAnswers++;
+		}	
 	},
 		//currently only updates container with buttons for answers
 		//next is updating the actual header text to be the question
-	emptyQuestionElement:function(){
+	clearQuestionAnswerElements:function(){
 
+		const game = this;
+
+		game.questionTextElement.empty();
+		game.answerContainerElement.empty();
 
 
 	},
@@ -65,7 +101,7 @@ function createQuestion(questionText,answer,answerPool){
 
 			answerPool.map(function(item){
 
-				questionElements.push("<p><a class=\"btn btn-primary btn-lg\" role=\"button\">"+ item + "</p>");
+				questionElements.push("<p><a class=\"btn btn-primary btn-lg btn-answer\" role=\"button\" value=\"" + item + "\">"+ item + "</p>");
 
 			});
 				
@@ -84,14 +120,22 @@ function createQuestion(questionText,answer,answerPool){
 
 
 
-$( document ).ready(function(){
-		
+
+
 	const game = createGame();
-	//create buttons based off of the answer list in our question object
 	game.addQuestion(createQuestion("Question 1?","No1", ["Yes1","No1"]));
 	game.addQuestion(createQuestion("Question 2?", "Yes2", ["Yes2","No2"]));
 	game.addQuestion(createQuestion("Question 3?", "Yes3", ["Yes3","No3"]));
-	game.updateQuestionElement(game.nextQuestion());
+
+	game.nextQuestion();
+
+	$( document ).ready(function(){
+		 $(document).on("click",".btn-answer",function(){
+
+		 	const selectedAnswer = $(this).attr("value");
+		 	game.checkIsCorrect(selectedAnswer,game.currentAnswer);
+		 	game.nextQuestion();
+		 });
 
 
 
